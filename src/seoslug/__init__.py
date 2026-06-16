0724708dfd3a689e5615f5ae94513e96331432ab
Emiliano Gandini Outeda <emiliano.gandini@protonmail.com>
2026-06-16 14:36:24 -0300

    Export exceptions and hooks from public API
    __init__.py now exposes SEOError, SEOConfigError, SEOEntityError,
    URLPolicyError, SEOPayloadError, hook, register_hook, clear_hooks,
    get_registered_hooks, and run_hooks in __all__.

--- src/seoslug/__init__.py
+++ src/seoslug/__init__.py
@@ -2,6 +2,14 @@
 
 from .builder import build_seo_payload
 from .config import SEOConfig, URLPolicy
+from .exceptions import (
+    SEOConfigError,
+    SEOEntityError,
+    SEOError,
+    SEOPayloadError,
+    URLPolicyError,
+)
+from .hooks import clear as clear_hooks, get_registered as get_registered_hooks, hook, register as register_hook, run as run_hooks
 from .normalization import normalize_path, normalize_public_url
 from .schemas import SEOEntity, SEOOverrides
 
@@ -13,4 +21,14 @@ __all__ = [
     "normalize_public_url",
     "normalize_path",
     "build_seo_payload",
+    "SEOError",
+    "SEOConfigError",
+    "SEOEntityError",
+    "URLPolicyError",
+    "SEOPayloadError",
+    "hook",
+    "register_hook",
+    "clear_hooks",
+    "get_registered_hooks",
+    "run_hooks",
 ]
